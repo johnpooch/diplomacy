63aded15da3980adcaf2b554a834614bbf4dda3d
John McDowell <johnmcdowell0801@gmail.com>
2018-07-09 18:26:00 +0000

    piece dictionary working. rendering to gamestate to board

--- game.py
+++ game.py
@@ -3,8 +3,23 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+game_state = {
+    "pieces": [
+        {
+            "territory": "par",
+            "owner": "france",
+            "piece_type": "a",
+        },
+        {
+            "territory": "bur",
+            "owner": "germany",
+            "piece_type": "f",
+        }
+    ]
+}
+
 @app.route("/")
 def get_index():
-    return render_template("index.html")
+    return render_template("index.html", game_state = game_state)
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
---
+++ place_piece.js
@@ -0,0 +1,6 @@
+// function place_piece(territory, owner) {
+//     var div = document.getElementsByTagName('div')[0];
+//     var piece = document.createElement("<div id='" + territory + "' class='piece " + owner + "-piece'/>");
+//     div.appendChild(piece);
+// }
+
