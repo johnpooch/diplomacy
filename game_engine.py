f34599ba0da44075fc52e628e62c984538471f8d
John McDowell <johnmcdowell0801@gmail.com>
2018-07-10 16:03:46 +0000

    going to watch flask videos and make accounts functioning

--- game_engine.py
+++ game_engine.py
@@ -3,15 +3,17 @@ from opening_game_state import opening_game_state
 
 available_nations = ["austria", "england", "france", "germany", "italy", "russia", "turkey"]
 
-def create_player(username):
-    nation = random.choice(available_nations)
-    available_nations.remove(nation)
-    opening_game_state["players"][username] = nation
-    if not available_nations:
-        game_state = initialise_game_state()
-    return game_state
-
 def initialise_game_state():
     game_state = opening_game_state
     print("game_state initialised")
     return game_state
+    
+def create_player(username):
+    nation = random.choice(available_nations)
+    available_nations.remove(nation)
+    opening_game_state["players"][username] = nation
+
+def get_game_state():
+    return(game_state)
+    
+game_state = initialise_game_state()
--- routing.py
+++ routing.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, redirect, render_template, request
 from flask_pymongo import PyMongo
-from game_engine import available_nations, initialise_game_state, create_player
+from game_engine import available_nations, initialise_game_state, create_player, get_game_state
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://diplomacy-johnpooch.c9users.io/myDatabase"
@@ -13,43 +13,13 @@ game_state = {}
 
 @app.route("/")
 def get_index():
-    if available_nations:
-        print(avaliable_nations)
-        return render_template("index.html")
-    else:
-        return render_template("board.html", game_state = game_state)
-    
+    game_state = get_game_state()
+    return render_template("board.html", game_state = game_state)
     
 @app.route("/login")
-def do_login():
-    username = request.args['username']
-    create_player(username)
-    return redirect(username)
+def login():
     
-
-@app.route("/<username>")
-def get_userpage(username):
-    return render_template("chat.html", logged_in_as=username, all_the_messages=messages)
-
-
-@app.route("/new", methods=["POST"])
-def add_message():
-    username = request.form['username']
-    text = request.form['message']
-    
-    words = text.split()
-    naughtyWords = set(["dang", "crud", "willy", "fudge"])
-    words = [ "*" * len(word) if word.lower() in naughtyWords else word for word in words]
-    
-    text = " ".join(map(str, words))
-    
-    message = {
-        'sender': username,
-        'body': text,
-    }
-    
-    messages.append(message)
-    return redirect(username)
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
