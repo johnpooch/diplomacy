ad26c81558a9dd9c890ed40ab6053fe8e6bbad91
John McDowell <johnmcdowell0801@gmail.com>
2018-07-25 17:51:56 +0000

    about to make the project class-based

--- process_orders.py
+++ process_orders.py
@@ -17,16 +17,18 @@ def change_places(orders):
                     })
     return True
 
+# =================================================================================================
+
+
+
+
+
 # RESOLVE CHALLENGES ==============================================================================
-    
-# find other pieces challenging territory ---------------------------------------------------------
     
 def find_other_pieces_challenging_territory(piece):
     print(piece["challenging"])
     territory = piece["challenging"]
     return [other_piece for other_piece in get_pieces() if other_piece["challenging"] == territory and piece["_id"] != other_piece["_id"]]
-
-# does piece have most support --------------------------------------------------------------------
 
 def piece_has_most_support(piece, pieces_to_compare):
     
@@ -38,8 +40,6 @@ def piece_has_most_support(piece, pieces_to_compare):
             piece["support"][piece["challenging"]] = 0
             
     return all(piece["support"][piece["challenging"]] > other_piece["support"][piece["challenging"]] for other_piece in pieces_to_compare)
-    
-# resolve challenges ------------------------------------------------------------------------------
 
 def resolve_challenges(orders):
     
@@ -92,15 +92,11 @@ def resolve_challenges(orders):
 
 # HELPERS =========================================================================================
 
-# get_origin_by_order -----------------------------------------------------------------------------
-
 def get_origin_by_order(order):
     for piece in get_pieces():
         if piece["_id"] == order["origin_id"]:
             return piece
     return False
-    
-# get_object_by_order -----------------------------------------------------------------------------
 
 def get_object_by_order(order):
     for piece in get_pieces():
@@ -108,24 +104,16 @@ def get_object_by_order(order):
             return piece
     return False
 
-# territory shares coast with origin --------------------------------------------------------------
-
 def territory_shares_coast_with_origin(origin, territory):
     for neighbour in territories[territory]["neighbours"]:
             if neighbour["name"] == origin:
                 return neighbour["shared_coast"]
-                
-# territory is special coast --------------------------------------------------------------------
 
 def territory_is_special_coast(territory):
     return "parent_territory" in territories[territory]
-    
-# territory has special coasts --------------------------------------------------------------------
 
 def territory_has_special_coasts(territory):
     return "special_coasts" in territories[territory]
-     
-# target is accessible by piece_type --------------------------------------------------------------
 
 def territory_is_accessible_by_piece_type(piece, territory):
     territory_type = territories[territory]["territory_type"]
@@ -134,24 +122,18 @@ def territory_is_accessible_by_piece_type(piece, territory):
     else:
          # refactor
         return territory_type == "water" or (territory_type == "coastal" and territory_shares_coast_with_origin(piece["territory"], territory)) or (territory_type == "coastal" and territories[piece["territory"]]["territory_type"] == "water")
-        
-# territory is neighbour --------------------------------------------------------------------------
 
 def territory_is_neighbour(origin, territory_to_check):
     
     neighbours = territories[origin]["neighbours"]
     # return true if the territory to check is one of its neighbours
     return any(neighbour["name"] == territory_to_check for neighbour in neighbours)
-        
-# object piece exists --------------------------------------------------------------------------
 
 def object_piece_exists(order, pieces):
     for piece in pieces:
         if order["object"] == piece["territory"]:
             return piece["_id"]
     return False
-        
-# piece exists and_belongs to user ----------------------------------------------------------------
 
 def piece_exists_and_belongs_to_user(order, pieces):
     for piece in pieces:
@@ -159,6 +141,13 @@ def piece_exists_and_belongs_to_user(order, pieces):
             return piece["_id"]
     write_to_log("\tinvalid move. there is no piece at {} or it does not belong to the user.".format(order["origin"]))
     return False
+    
+def get_orders():
+    return [order for order in mongo.db.orders.find()]
+    
+def get_pieces():
+    return [piece for piece in mongo.db.pieces.find()]
+    
 
 # =================================================================================================
 
@@ -167,8 +156,6 @@ def piece_exists_and_belongs_to_user(order, pieces):
 
 
 # PROCESS SUPPORT =================================================================================
-
-# update support ----------------------------------------------------------------------------------
 
 def update_support(order):
     object_piece = get_object_by_order(order)
@@ -179,31 +166,17 @@ def update_support(order):
     else:
         object_supports.update({order["target"]: 1})
     mongo.db.pieces.update({"territory": order["object"]}, {"$set":{"support": object_supports}})
-        
-# support is valid --------------------------------------------------------------------------------
 
 def support_is_valid(order):
     piece = get_origin_by_order(order)
     return territory_is_neighbour(order["origin"], order["target"]) and territory_is_accessible_by_piece_type(piece, order["target"])
-    
-# process support ---------------------------------------------------------------------------------
 
 def process_support(order):
     if support_is_valid(order):
         update_support(order)
         return True
     return False
-    
-# get orders --------------------------------------------------------------------------------------
 
-def get_orders():
-    return [order for order in mongo.db.orders.find()]
-    
-# get pieces --------------------------------------------------------------------------------------
-    
-def get_pieces():
-    return [piece for piece in mongo.db.pieces.find()]
-    
 # =================================================================================================
 
 
@@ -212,8 +185,6 @@ def get_pieces():
 
 
 # PROCESS MOVE ====================================================================================
-
-# target acceissible by convoy --------------------------------------------------------------------
 
 def target_accessible_by_convoy(order, piece, origin):
     for neighbour in territories[origin]["neighbours"]:
@@ -224,13 +195,9 @@ def target_accessible_by_convoy(order, piece, origin):
                 return target_accessible_by_convoy(order, piece, neighbour["name"])
     return False
 
-# move is valid -----------------------------------------------------------------------------------
-
 def move_is_valid(order):
     piece = get_origin_by_order(order)
     return (target_accessible_by_convoy(order, piece, order["origin"]) or territory_is_neighbour(order["origin"], order["target"])) and territory_is_accessible_by_piece_type(piece, order["target"])
-        
-# process move ------------------------------------------------------------------------------------
 
 def process_move(order):
     if not move_is_valid(order):
@@ -248,12 +215,8 @@ def process_move(order):
 
 # PROCESS CONVOY ==================================================================================
 
-# piece is on water --------------------------------------------------------------------------------
-
 def piece_is_on_water(piece):
     return territories[piece["territory"]]["territory_type"] == "water"
-    
-# process convoy ----------------------------------------------------------------------------------
 
 def process_convoy(order):
     piece = get_origin_by_order(order)
@@ -309,35 +272,26 @@ def update_challenges(orders):
 
 # END TURN ========================================================================================
 
-# save orders to history
 def attach_pieces_to_order(orders, pieces):
     for order in orders:
         mongo.db.orders.update({"_id": order["_id"]}, {"$set": {"origin_id": piece_exists_and_belongs_to_user(order, pieces)}})
         mongo.db.orders.update({"_id": order["_id"]}, {"$set": {"object_id": object_piece_exists(order, pieces)}})
-
-# save orders to history
 
 def save_orders_to_history():
     order_history = mongo.db.order_history
     for order in get_orders():
         order_history.insert(order)
 
-# check for retreats ------------------------------------------------------------------------------
 def check_for_retreats():
     return any(piece["must_retreat"] for piece in get_pieces())
-    
-# check for builds --------------------------------------------------------------------------------
+
 def check_for_builds():
     return True
-    
-# increment year ----------------------------------------------------------------------------------
 
 def increment_year():
     new_year = (mongo.db.game_properties.find_one()["year"] + 1)
     write_to_log("year incremented. new year: {}".format(new_year))
     mongo.db.game_properties.update({}, {"$set": {"year": new_year}})
-
-# change phase ------------------------------------------------------------------------------
 
 def change_phase(current_phase):
     
@@ -373,25 +327,17 @@ def change_phase(current_phase):
         
     write_to_log("\nphase updated from {0} to {1}.".format(current_phase, update_phase))
     mongo.db.game_properties.update_one({}, {"$set": {"phase": update_phase}})
-    
-# unfinalise_users --------------------------------------------------------------------------------
 
 def unfinalise_users():
     mongo.db.users.update({}, {"$set": {"orders_finalised": False}}, multi=True)
     write_to_log("user orders unfinalised\n")
     
-# write to log ------------------------------------------------------------------------------------
-    
 def write_to_log(string):
     with open('log.txt', 'a') as log:
         log.write(string + '\n')
         
-# clear log ---------------------------------------------------------------------------------------
-        
 def clear_log():
     open('log.txt', 'w').close()
-
-# end turn ----------------------------------------------------------------------------------------
 
 def end_turn():
     
