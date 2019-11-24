ebf20f95d82ab39f81e491b3d96fc8cac051925b
Tim Wyse <wysetm@gmail.com>
2019-11-24 20:51:00 +0100

    add_tests_to_territory

--- core/adjudicator/territory.py
+++ core/adjudicator/territory.py
@@ -1,13 +1,42 @@
-# TODO Come up with some sort of registry to allow for many to many
 # relationships between territories. Look into observer pattern
 
 
 class Territory:
 
-    def __init__(self, name, nationality):
+    all_territories = []
+
+    def __init__(self, id, name, nationality, neighbour_ids):
+        self.id = id
         self.name = name
         self.nationality = nationality
-        self.neighbours = []
+        self.neighbour_ids = neighbour_ids
+        self._neighbours = []
+        self.__class__.all_territories.append(self)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f'{self.name} - {self.__class__.__name__}'
+
+    @property
+    def neighbours(self):
+        """
+        Gets the neighbours of the territory. Neighbours are cached in the
+        `self._neighbours` attribute. This avoids unnecessary iteration because
+        the neighbours of a territory never change during adjudication.
+
+        Returns:
+            * A list of `Territory` instances.
+        """
+        if not self._neighbours:
+            for t in self.__class__.all_territories:
+                if t.id in self.neighbour_ids:
+                    self._neighbours.append(t)
+        return self._neighbours
+
+    def adjacent_to(self, territory):
+        return territory in self.neighbours
 
 
 class LandTerritory(Territory):
@@ -16,9 +45,26 @@ class LandTerritory(Territory):
 
 class CoastalTerritory(LandTerritory):
 
-    def __init__(self, name, nationality):
-        super().__init__(name, nationality)
-        self.shared_coasts = []
+    def __init__(self, id, name, nationality, neighbour_ids, shared_coast_ids):
+        super().__init__(id, name, nationality, neighbour_ids)
+        self.shared_coast_ids = shared_coast_ids
+        self._shared_coasts = []
+
+    @property
+    def shared_coasts(self):
+        """
+        Gets the shared_coasts of the territory. Neighbours are cached in the
+        `self._shared_coasts` attribute. This avoids unnecessary iteration because
+        the neighbours of a territory never change during adjudication.
+
+        Returns:
+            * A list of `Territory` instances.
+        """
+        if not self._shared_coasts:
+            for t in self.__class__.all_territories:
+                if t.id in self.shared_coast_ids:
+                    self._shared_coasts.append(t)
+        return self._shared_coasts
 
 
 class InlandTerritory(LandTerritory):
@@ -26,9 +72,11 @@ class InlandTerritory(LandTerritory):
 
 
 class ComplexTerritory(CoastalTerritory):
-    def __init__(self, name, nationality):
-        super().__init__(name, nationality)
-        self.named_coasts = []
+
+    def __init__(self, id, name, nationality, neighbour_ids, shared_coast_ids,
+                 named_coast_ids):
+        super().__init__(id, name, nationality, neighbour_ids, shared_coast_ids)
+        self.named_coast_ids = named_coast_ids
 
 
 class SeaTerritory(Territory):
