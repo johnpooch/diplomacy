da8abbb50cb5d83390e448ce3338f068fb16cac2
johnpooch <johnmcdowell0801@gmail.com>
2019-11-30 16:30:09 +0000

    Whitespace

--- service/permissions.py
+++ service/permissions.py
@@ -18,7 +18,6 @@ class IsCreatedByOrReadOnly(permissions.BasePermission):
     Custom permission to only allow the user which created an object to edit
     it.
     """
-
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
