e612e2867a0b4084ff29b34922c6264f3e31254c
John McDowell <john.mcdowell@gamer-network.net>
2020-05-16 15:25:23 +0100

    Add available orders test

--- core/tests/test_retreat_and_disband.py
+++ core/tests/test_retreat_and_disband.py
@@ -2,26 +2,33 @@ from django.test import TestCase
 
 from core import models
 from core import factories
-from core.models.base import Phase, Season
+from core.models.base import OrderType, Phase, Season
 
 
 class TestRetreatAndDisband(TestCase):
 
     def setUp(self):
         variant = factories.StandardVariantFactory()
-        users = []
-        for i in range(7):
-            users.append(factories.UserFactory())
+        user = factories.UserFactory()
         self.game = models.Game.objects.create(
             name='Test game',
             variant=variant,
             num_players=7,
-            created_by=users[0],
+            created_by=user,
         )
-        self.game.participants.add(*users)
+        self.game.participants.add(user)
+        self.retreat_turn = models.Turn.objects.create(
+            game=self.game,
+            phase=Phase.RETREAT_AND_DISBAND,
+            season=Season.FALL,
+            year=1901,
+        )
 
     def test_only_retreat_and_disband_orders_are_available(self):
-        pass
+        self.assertEqual(
+            self.retreat_turn.possible_order_types,
+            [OrderType.RETREAT, OrderType.DISBAND]
+        )
 
     def test_pieces_to_order_equal_to_num_pieces_which_must_retreat(self):
         pass
