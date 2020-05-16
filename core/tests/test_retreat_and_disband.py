017a5d59d7dde132749b21edd138802fa536e5dd
John McDowell <john.mcdowell@gamer-network.net>
2020-05-16 15:44:58 +0100

    Add num pieces to order test
    Tests that the number of pieces a nation can order during a retreat
    phase is equal to the number of its pieces which must retreat.

--- core/tests/test_retreat_and_disband.py
+++ core/tests/test_retreat_and_disband.py
@@ -2,21 +2,21 @@ from django.test import TestCase
 
 from core import models
 from core import factories
-from core.models.base import OrderType, Phase, Season
+from core.models.base import OrderType, Phase, PieceType, Season
 
 
 class TestRetreatAndDisband(TestCase):
 
     def setUp(self):
-        variant = factories.StandardVariantFactory()
-        user = factories.UserFactory()
+        self.variant = factories.StandardVariantFactory()
+        self.user = factories.UserFactory()
         self.game = models.Game.objects.create(
             name='Test game',
-            variant=variant,
+            variant=self.variant,
             num_players=7,
-            created_by=user,
+            created_by=self.user,
         )
-        self.game.participants.add(user)
+        self.game.participants.add(self.user)
         self.retreat_turn = models.Turn.objects.create(
             game=self.game,
             phase=Phase.RETREAT_AND_DISBAND,
@@ -31,7 +31,26 @@ class TestRetreatAndDisband(TestCase):
         )
 
     def test_pieces_to_order_equal_to_num_pieces_which_must_retreat(self):
-        pass
+        france = self.variant.nations.get(name='France')
+        nation_state = models.NationState.objects.create(
+            turn=self.retreat_turn,
+            nation=france,
+            user=self.user
+        )
+        self.assertEqual(nation_state.pieces_to_order.count(), 0)
+        piece = models.Piece.objects.create(
+            game=self.game,
+            nation=france,
+            type=PieceType.ARMY,
+        )
+        piece_state = models.PieceState.objects.create(
+            piece=piece,
+            turn=self.retreat_turn,
+        )
+        self.assertEqual(nation_state.pieces_to_order.count(), 0)
+        piece_state.must_retreat = True
+        piece_state.save()
+        self.assertEqual(nation_state.pieces_to_order.count(), 1)
 
     def test_pieces_which_are_disbanded_are_removed_from_the_game(self):
         pass
