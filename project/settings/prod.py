2e23fc931f82ad491c3454d0cf13bd3e2e210d20
John McDowell <john.mcdowell@gamer-network.net>
2020-06-21 17:34:22 +0100

    Redo migrations

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -4,6 +4,10 @@ import dj_database_url
 
 from .base import *
 
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
