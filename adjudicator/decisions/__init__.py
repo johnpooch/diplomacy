8cfbf6fdad60ca0a6548b18eada41474636f108c
John McDowell <john.mcdowell@gamer-network.net>
2020-06-21 13:55:32 +0100

    Remove redundant imports

--- adjudicator/decisions/__init__.py
+++ adjudicator/decisions/__init__.py
@@ -1,20 +1,12 @@
 from .base import Outcomes
-from .legal import BuildLegal, ConvoyLegal, MoveLegal, SupportLegal
 from .defend_strength import DefendStrength
 from .hold_strength import HoldStrength
 from .attack_strength import AttackStrength
-from .support import Support
 from .path import Path
 from .prevent_strength import PreventStrength
 
 __all__ = [
     'Outcomes',
-
-    # Legal
-    'BuildLegal',
-    'ConvoyLegal',
-    'MoveLegal',
-    'SupportLegal',
 
     # Strength
     'DefendStrength',
