27b951b70e5626a3d65c868fe4d1a361dcb83992
John McDowell <john.mcdowell@gamer-network.net>
2020-11-07 20:22:26 +0000

    More docker changes

--- .docker/bin/_start.sh
+++ .docker/bin/_start.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+if (( $RUNTIMEINSTALL )); then
+    echo Installing any changed requirements...
+    pushd $SITE_NAME
+    install-requirements $ENV
+    popd
+fi
+
 if [ $RUNMIGRATIONS ]; then
     echo Running migrations...
     manage migrate
@@ -6,4 +15,3 @@ fi
 for target in $WAITFOR; do
     wait-for-it $target --timeout=120
 done
-
