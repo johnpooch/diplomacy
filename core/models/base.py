7baf11ec698545704c953d5df2c22624835a7298
John McDowell <john.mcdowell@gamer-network.net>
2020-11-09 15:57:11 +0000

    Use timespans for deadlines in models.base

--- core/models/base.py
+++ core/models/base.py
@@ -1,6 +1,8 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 
+from core.utils.date.timespan import timespans
+
 
 class NationChoiceMode:
     RANDOM = 'random'
@@ -14,19 +16,19 @@ class NationChoiceMode:
 
 
 class DeadlineFrequency:
-    TWELVE_HOURS = 'twelve_hours'
-    TWENTY_FOUR_HOURS = 'twenty_four_hours'
-    TWO_DAYS = 'two_days'
-    THREE_DAYS = 'three_days'
-    FIVE_DAYS = 'five_days'
-    SEVEN_DAYS = 'seven_days'
+    TWELVE_HOURS = timespans.TWELVE_HOURS.db_string
+    TWENTY_FOUR_HOURS = timespans.TWENTY_FOUR_HOURS.db_string
+    TWO_DAYS = timespans.TWO_DAYS.db_string
+    THREE_DAYS = timespans.THREE_DAYS.db_string
+    FIVE_DAYS = timespans.FIVE_DAYS.db_string
+    SEVEN_DAYS = timespans.SEVEN_DAYS.db_string
     CHOICES = (
-        (TWELVE_HOURS, '12 hours'),
-        (TWENTY_FOUR_HOURS, '24 hours'),
-        (TWO_DAYS, '2 days'),
-        (THREE_DAYS, '3 days'),
-        (FIVE_DAYS, '5 days'),
-        (SEVEN_DAYS, '7 days'),
+        timespans.TWELVE_HOURS.as_choice,
+        timespans.TWENTY_FOUR_HOURS.as_choice,
+        timespans.TWO_DAYS.as_choice,
+        timespans.THREE_DAYS.as_choice,
+        timespans.FIVE_DAYS.as_choice,
+        timespans.SEVEN_DAYS.as_choice,
     )
 
 
