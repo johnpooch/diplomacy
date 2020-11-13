987c720fec593c69cff497039b2b8714fb8848ef
John McDowell <john.mcdowell@gamer-network.net>
2020-11-13 18:58:10 +0000

    Update tests for NationState

--- core/tests/test_nation_state.py
+++ core/tests/test_nation_state.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from django.utils import timezone
 
+from core import models
 from core.tests import DiplomacyTestCaseMixin
 
 
@@ -13,61 +13,45 @@ class TestNationState(TestCase, DiplomacyTestCaseMixin):
         self.turn = self.create_test_turn(game=self.game)
         self.user = self.create_test_user()
 
-    def create_nation_state(self, **kwargs):
-        return self.create_test_nation_state(
-            nation=self.nation,
+    def test_user_surrendering_no_user(self):
+        nation_state = self.create_test_nation_state(
             turn=self.turn,
+            nation=self.nation,
+            user=None
+        )
+        self.assertFalse(nation_state.user_surrendering)
+
+    def test_user_surrendering_not_surrendering(self):
+        nation_state = self.create_test_nation_state(
+            turn=self.turn,
+            nation=self.nation,
             user=self.user,
-            **kwargs,
         )
+        self.assertFalse(nation_state.user_surrendering)
 
-    def test_surrender_no_commit(self):
-        nation_state = self.create_test_nation_state()
-        nation_state.pk = None
-        nation_state = nation_state.surrender(commit=False)
-        self.assertIsNone(nation_state.pk)
-
-    def test_surrender(self):
-        nation_state = self.create_test_nation_state()
-        nation_state = nation_state.surrender()
-        self.assertTrue(nation_state.surrendered)
-        self.assertTrue(nation_state.surrendered_at)
-
-    def test_cancel_surrender_no_commit(self):
+    def test_user_surrendering(self):
         nation_state = self.create_test_nation_state(
-            surrendered=True,
-            surrendered_at=timezone.now(),
+            turn=self.turn,
+            nation=self.nation,
+            user=self.user,
         )
-        nation_state.pk = None
-        nation_state = nation_state.cancel_surrender(commit=False)
-        self.assertIsNone(nation_state.pk)
+        models.Surrender.objects.create(user=self.user, turn=self.turn)
+        self.assertTrue(nation_state.user_surrendering)
 
-    def test_cancel_surrender(self):
+    def test_civil_disorder_no_user(self):
         nation_state = self.create_test_nation_state(
-            surrendered=True,
-            surrendered_at=timezone.now(),
+            turn=self.turn,
+            nation=self.nation,
+            user=None,
         )
-        nation_state = nation_state.cancel_surrender()
-        self.assertFalse(nation_state.surrendered)
-        self.assertIsNone(nation_state.surrendered_at)
+        models.Surrender.objects.create(user=self.user, turn=self.turn)
+        self.assertTrue(nation_state.civil_disorder)
 
-    def test_toggle_surrender_not_current_turn(self):
-        nation_state = self.create_test_nation_state()
-        nation_state.turn.current_turn = False
-        with self.assertRaises(ValueError):
-            nation_state = nation_state.toggle_surrender()
-
-    def test_toggle_surrender_not_surrendered(self):
-        nation_state = self.create_test_nation_state()
-        nation_state = nation_state.toggle_surrender()
-        self.assertTrue(nation_state.surrendered)
-        self.assertTrue(nation_state.surrendered_at)
-
-    def test_toggle_surrender_surrendered(self):
+    def test_civil_disorder_user_surrendered(self):
         nation_state = self.create_test_nation_state(
-            surrendered=True,
-            surrendered_at=timezone.now(),
+            turn=self.turn,
+            nation=self.nation,
+            user=self.user,
         )
-        nation_state = nation_state.toggle_surrender()
-        self.assertFalse(nation_state.surrendered)
-        self.assertIsNone(nation_state.surrendered_at)
+        models.Surrender.objects.create(user=self.user, turn=self.turn)
+        self.assertTrue(nation_state.civil_disorder)
