7b0003973cf2d4cb8709c2db5595b8aa81e4be12
John McDowell <john.mcdowell@gamer-network.net>
2020-11-20 22:54:49 +0000

    Set draw to expired on turn end

--- core/models/turn.py
+++ core/models/turn.py
@@ -98,6 +98,10 @@ class TurnManager(models.Manager):
             territory_state.save()
         for nation_state in previous_turn.nationstates.all():
             nation_state.copy_to_new_turn(new_turn)
+        for draw in previous_turn.draws.all():
+            if draw.status ==  DrawStatus.PROPOSED:
+                draw.status = DrawStatus.EXPIRED
+                draw.save()
         new_turn.season, new_turn.phase, new_turn.year = \
             previous_turn.get_next_season_phase_and_year(new_turn)
         new_turn.save()
