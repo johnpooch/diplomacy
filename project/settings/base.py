265ddfe186ea30b887f3dae1015f40d376de2401
johnm <johnmcdowell0801@gmail.com>
2021-04-21 16:53:41 +0100

    Add authentication backend that accepts email or username

--- project/settings/base.py
+++ project/settings/base.py
@@ -144,7 +144,7 @@ REST_FRAMEWORK = {
 }
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
+    'core.backends.EmailBackend',
 )
 
 # Static files (CSS, JavaScript, Images)
